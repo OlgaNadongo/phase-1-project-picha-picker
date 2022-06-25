393766cf292e74f764296f234c569f66b6b77cf3
Olga Nadongo <winnienadongo@gmail.com>
2022-06-25 14:40:15 +0300

    adding event listener to image button

---
+++ app.js
@@ -0,0 +1,11 @@
+let clientID ="-2yT9-B7fKq2ROF16qevFLw9OeXkPFU93B7ssClGeh0";
+let finalPoint=`https://api.unsplash.com/photos/random/?client_id=${clientID}`;
+
+
+let imageItem=document.querySelector("#imageresult")
+
+
+//add click event to imagebutton such that when clicked it displays a random image
+let imageBtn=document.querySelector("#imgbtn")
+
+imageBtn.addEventListener("click", showRandomImage)
